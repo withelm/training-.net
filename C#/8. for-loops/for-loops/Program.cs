edd4ae904398bba179d4e775c239a9b5778bfa18
withelm <withelm@gmail.com>
2020-07-11 19:11:53 +0200

    8 - 9

---
+++ C#/8. for-loops/for-loops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+
+namespace for_loops
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 0; j <= 24; j += i)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#/9. arrays-collections/arrays-collections/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arrays_collections
+{
+    internal class Program
+    {
+        private static List<string> ContentList = new List<string>();
+
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                ShowInstruction();
+                string line = Console.ReadLine();
+
+                if (line[0] == '+')
+                {
+                    ContentList.Add(line.Substring(2, line.Length - 2));
+                    continue;
+                }
+                if (line[0] == '-' && line[1] == ' ')
+                {
+                    line = line.Substring(2, line.Length - 2);
+                    ContentList.Remove(line);
+                    continue;
+                }
+                if (line[0] == '-' && line[1] == '-')
+                {
+                    ContentList.Clear();
+                    continue;
+                }
+            }
+        }
+
+        private static void ShowInstruction()
+        {
+            Console.WriteLine("Enter command (+ item, - item, or -- to clear)):");
+            if (ContentList.Count > 0)
+            {
+                Console.WriteLine("Current list:");
+                ContentList.ForEach(x => Console.WriteLine($"> {x}"));
+            }
+        }
+    }
+}
